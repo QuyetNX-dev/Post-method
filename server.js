60c302d118b88d3771ee8eda50353b36b8e6d9dc
Glitch (hello-express) <none>
2020-04-21 14:47:22 +0000

    ❄️🌞 Checkpoint ./package.json copy:15245433/629 ./package.json:15245433/1279 ./server.js:15245433/1407 ./LICENSE.md:15245433/1068 ./CODE_OF_CONDUCT.md:15245433/6749 ./views/index.pug:15245433/76

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
-// server.js
-// where your node app starts
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+app.set('view engine', 'pug');
+app.set('views', './views')
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+app.get("/", (req, res) => {
+  res.render('index.pug',{
+    name: 'Quety'
+  })
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+app.get("/", (req, res) => {
+  res.render('todos.pug',{
+    users: [
+      {id: 1, name: 'đẹp trai'},
+      
+    ]
+  })
 });
 
 // listen for requests :)
